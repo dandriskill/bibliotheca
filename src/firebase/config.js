5218ade7d9a0941ae5fd0406132aa225cf412b4e
Daniel Driskill <daniel.driskill@its-MacBook-Pro-6.local>
2020-09-05 10:25:50 -0600

    Get rid of firestore dev mode console warning.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyBC832GvoAab2XnN2jiZBzPGskZP561XVk",

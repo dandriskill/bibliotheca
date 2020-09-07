9817f3cbdce0290d62f92af8fc34c6a2ee5b3180
Daniel Driskill <daniel.driskill@its-MacBook-Pro-6.local>
2020-09-07 11:41:20 -0600

    Add the rest of the utils tests.

--- src/utils.test.js
+++ src/utils.test.js
@@ -12,11 +12,39 @@ describe('utils', () => {
     expect(checkForUrls('J.R.R.')).toBe(false);
   });
 
-  xtest('upperFirst()', () => {
-    
+  test('upperFirst()', () => {
+    expect(upperFirst('daniel')).toBe('Daniel');
+    expect(upperFirst('daniel driskill')).toBe('Daniel Driskill');
+    expect(upperFirst('daniel k. driskill')).toBe('Daniel K. Driskill');
   });
 
-  xtest('filterBooks()', () => {
-    
+  test('filterBooks()', () => {
+    const books = [{
+      title: 'The Prince',
+      author: 'Niccolo Machiavelli',
+    },
+    {
+      title: 'Republic',
+      author: 'Plato',
+    },
+    {
+      title: 'Oddyssey',
+      author: 'Homer',
+    }];
+
+    expect(filterBooks('', books)).toStrictEqual(books);
+    expect(filterBooks('p', books)).toStrictEqual([{
+      title: 'The Prince',
+      author: 'Niccolo Machiavelli',
+    },
+    {
+      title: 'Republic',
+      author: 'Plato',
+    }]);
+    expect(filterBooks('pr', books)).toStrictEqual([{
+      title: 'The Prince',
+      author: 'Niccolo Machiavelli',
+    }]);
+    expect(filterBooks('', books)).toStrictEqual(books);
   });
 });
